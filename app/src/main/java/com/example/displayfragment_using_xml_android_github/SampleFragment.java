75687135595bc45a518202e9bd72001955477cfc
Aman Sharma <amansharma.11@hotmail.com>
2019-06-26 23:28:27 +0530

    commit

---
+++ app/src/main/java/com/example/displayfragment_using_xml_android_github/SampleFragment.java
@@ -0,0 +1,29 @@
+package com.example.displayfragment_using_xml_android_github;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SampleFragment extends Fragment {
+
+
+    public SampleFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sample, container, false);
+    }
+
+}
