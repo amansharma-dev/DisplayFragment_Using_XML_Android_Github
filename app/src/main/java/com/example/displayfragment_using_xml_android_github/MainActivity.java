9c9331b615f5916ce8cd764738dc56387e01d338
Aman Sharma <amansharma.11@hotmail.com>
2019-06-26 21:19:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/displayfragment_using_xml_android_github/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.displayfragment_using_xml_android_github;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
